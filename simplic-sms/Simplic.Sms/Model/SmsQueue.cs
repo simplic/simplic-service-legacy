aea561e2d63efa89423fa4d0654b82b41a3176fb
Yavuz Günay <yavuzgunay@gmail.com>
2018-06-04 11:22:30 +0200

    added sms service core

---
+++ simplic-sms/Simplic.Sms/Model/SmsQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Simplic.Sms
+{
+    /// <summary>
+    /// Sms Queue object
+    /// </summary>
+    public class SmsQueue
+    {
+        /// <summary>
+        /// Gets or sets the Id of the sms queue object
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number of the sms queue object
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Gets or sets the text of the sms queue object
+        /// </summary>
+        public string Body { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the sms queue object
+        /// </summary>
+        public SmsStatus Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the provider response of the sms queue object
+        /// </summary>
+        public string ProviderResponse { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created time of the sms queue object
+        /// </summary>
+        public DateTime? CreateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sent time of the sms queue object
+        /// </summary>
+        public DateTime? SentTime { get; set; }
+    }
+}
---
+++ simplic-sms/Simplic.Sms/Model/SmsStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Simplic.Sms
+{
+    /// <summary>
+    /// Sms Queue status
+    /// </summary>
+    public enum SmsStatus
+    {
+        InQueue = 0,
+        Sent = 1,
+        Failed = 2
+    }
+}
