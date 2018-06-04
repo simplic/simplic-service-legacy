780a2664c42c8437219f707158ceafe74fbb8460
Yavuz Günay <yavuzgunay@gmail.com>
2018-06-04 11:13:39 +0200

    added icon service core

---
+++ simplic-icon/Simplic.Icon/Model/Icon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+namespace Simplic.Icon
+{
+    /// <summary>
+    /// Represents an icon saved in the database
+    /// </summary>
+    public class Icon
+    {
+        #region Private Members
+        private BitmapImage iconBlobAsImage;
+        #endregion
+
+        #region Public Properties
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public byte[] IconBlob { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public DateTime? UpdateDateTime { get; set; }        
+        public BitmapImage IconBlobAsImage
+        {
+            get
+            {
+                if (iconBlobAsImage == null)
+                {
+                    if (IconBlob == null || IconBlob.Length <= 0) return null;
+
+                    var img = new BitmapImage();
+                    using (var ms = new System.IO.MemoryStream(IconBlob))
+                    {
+                        ms.Position = 0;
+                        img.BeginInit();
+                        img.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                        img.CacheOption = BitmapCacheOption.OnLoad;
+                        img.UriSource = null;
+                        img.StreamSource = ms;
+                        img.EndInit();
+                    }
+                    
+                    img.Freeze();
+
+                    iconBlobAsImage = img;
+                }
+
+                return iconBlobAsImage;
+            }
+        }
+        #endregion
+    }
+}
---
+++ simplic-icon/Simplic.Icon/Model/SystemIconSize.cs
@@ -0,0 +1,18 @@
+﻿namespace Simplic.Icon
+{
+    /// <summary>
+    /// Icon size for system icons
+    /// </summary>
+    public enum SystemIconSize
+    {
+        /// <summary>
+        /// Large system icon 32x32
+        /// </summary>
+        Large = 0,
+
+        /// <summary>
+        /// Small icon size 16x16
+        /// </summary>
+        Small = 1
+    }
+}
