01a372870fce76dab93417462f4f9033a39b63cb
Yavuz Günay <yavuzgunay@gmail.com>
2018-06-04 10:47:02 +0200

    added authentication, authorization services

---
+++ simplic-authentication/Simplic.Authentication/Exception/LoginFailedType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.Authentication
+{
+    /// <summary>
+    /// Type of login failed
+    /// </summary>
+    public enum LoginFailedType
+    {
+        /// <summary>
+        /// User not found
+        /// </summary>
+        UserNotFound,
+
+        /// <summary>
+        /// User is inactive
+        /// </summary>
+        UserNotActive,
+
+        /// <summary>
+        /// No password
+        /// </summary>
+        PasswordNotEntered,
+
+        /// <summary>
+        /// No username
+        /// </summary>
+        UserNameNotEntered,
+
+        /// <summary>
+        /// Invalid credentials
+        /// </summary>
+        InvalidCredentials
+    }
+}
---
+++ simplic-authentication/Simplic.Authorization/Model/AccessRightType.cs
@@ -0,0 +1,9 @@
+﻿namespace Simplic.Authorization
+{
+    public enum AccessRightType
+    {
+        Read = 0,
+        Write = 1,
+        Full = 2
+    }
+}
