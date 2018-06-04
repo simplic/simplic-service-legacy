46abf6c3eb6bfc3f885fd7897109a5256d087931
Yavuz Günay <yavuzgunay@gmail.com>
2018-06-04 10:21:26 +0200

    project structure clean up

---
+++ simplic-sql/Simplic.Sql/TypeHandler/PreciseDecimalHandler.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using Simplic.Data;
+using System.Data;
+
+namespace Simplic.Sql
+{
+    /// <summary>
+    /// <see cref="PreciseDecimal"/> dapper type handler
+    /// </summary>
+    internal class PreciseDecimalHandler : SqlMapper.TypeHandler<PreciseDecimal>
+    {
+        public override void SetValue(IDbDataParameter parameter, PreciseDecimal value)
+        {
+            parameter.Value = (double)value;
+        }
+
+        public override PreciseDecimal Parse(object value)
+        {
+            return new PreciseDecimal((double)value);
+        }
+    }
+}
---
+++ simplic-sql/Simplic.Sql/TypeHandler/TypeHandler.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+
+namespace Simplic.Sql
+{
+    /// <summary>
+    /// Typehandler
+    /// </summary>
+    public static class TypeHandler
+    {
+        /// <summary>
+        /// Register dapper type handler
+        /// </summary>
+        public static void RegisterTypeHandler()
+        {
+            SqlMapper.AddTypeHandler(new PreciseDecimalHandler());
+        }
+    }
+}
