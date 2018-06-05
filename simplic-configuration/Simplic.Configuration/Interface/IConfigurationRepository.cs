d77236fd6b3621ef609551ab90e6ab191a946831
Yavuz Günay <yavuzgunay@gmail.com>
2018-06-05 10:42:27 +0200

    added configuration core project

---
+++ simplic-configuration/Simplic.Configuration/Interface/IConfigurationRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Simplic.Configuration
+{
+    public interface IConfigurationRepository
+    {
+        /// <summary>
+        /// Gets a configuration value
+        /// </summary>
+        /// <param name="plugInName">Plugin name</param>
+        /// <param name="userName">User name</param>
+        /// <param name="configurationName">Configuration name</param>
+        /// <returns>Configuration value</returns>
+        string GetValue(string pluginName, string userName, string configurationName);
+        
+        /// <summary>
+        /// Sets a configuration value (saves in the db)
+        /// </summary>
+        /// <param name="pluginName">Plugin name</param>
+        /// <param name="userName">User name</param>
+        /// <param name="configurationName">Configuration name</param>
+        /// <param name="configurationValue">Configuration value</param>
+        void SetValue(string pluginName, string userName, string configurationName, string configurationValue);
+
+        /// <summary>
+        /// Gets a list configuration values
+        /// </summary>
+        /// <typeparam name="T">Expected type</typeparam>
+        /// <param name="plugInName">Plugin name</param>
+        /// <param name="userName">User name</param>
+        /// <returns>A list configuration values</returns>
+        IEnumerable<ConfigurationValue> GetValues<T>(string plugInName, string userName);
+    }
+}
---
+++ simplic-configuration/Simplic.Configuration/Model/ConfigurationValue.cs
@@ -0,0 +1,77 @@
+﻿using Simplic.Cache;
+
+namespace Simplic.Configuration
+{
+    /// <summary>
+    /// Konfigurationswert
+    /// </summary>
+    public class ConfigurationValue : ICacheObject
+    {
+        private string configName;
+        private string plugInName;
+        private string userName;
+        private object value;
+
+        /// <summary>
+        /// Neues Configuration-Wert erstellen
+        /// </summary>
+        /// <param name="ConfigName">Name der Einstellung</param>
+        /// <param name="PlugInName">PlugIn-Name</param>
+        /// <param name="UserName">UserName</param>
+        /// <param name="Value">Wert</param>
+        public ConfigurationValue(string ConfigName, string PlugInName, string UserName, object Value)
+        {
+            configName = ConfigName;
+            plugInName = PlugInName;
+            userName = UserName;
+            value = Value;
+        }
+
+        public void OnRemove()
+        {
+
+        }
+
+        /// <summary>
+        /// Generiert den eindeutigen Key eines ConvifurationValue - ICacheable
+        /// </summary>
+        /// <param name="ConfigurationName"></param>
+        /// <param name="PlugInName"></param>
+        /// <param name="UserName"></param>
+        /// <returns></returns>
+        internal static string GetKeyName(string ConfigurationName, string PlugInName, string UserName)
+        {
+            return (ConfigurationName + PlugInName + UserName).ToLower().Trim();
+        }
+
+        public object Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+
+        /// <summary>
+        /// Eindeutiger Schlüssel
+        /// </summary>
+        public string Key
+        {
+            get { return GetKeyName(configName, plugInName, userName); }
+        }
+
+        /// <summary>
+        /// Gets the configuration name
+        /// </summary>
+        public string ConfigName
+        {
+            get
+            {
+                return configName;
+            }
+        }
+
+        public string CacheKey
+        {
+            get { return nameof(ConfigurationValue); }
+        }
+    }
+}
