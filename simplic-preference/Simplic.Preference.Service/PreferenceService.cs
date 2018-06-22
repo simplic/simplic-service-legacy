cb23eebaae2c4a2027fa3e0c68adfa7e3932ae4d
Yavuz Günay <yavuzgunay@gmail.com>
2018-06-22 16:08:50 +0200

    created preference service with dictionary that holds key value pairs

---
+++ simplic-preference/Simplic.Preference.Service/PreferenceService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Simplic.Preference.Service
+{
+    /// <summary>
+    /// Preference service holds a list of key value pairs (Dictionary)
+    /// </summary>
+    public class PreferenceService : IPreferenceService
+    {
+        /// <summary>
+        /// Gets or sets the preferences
+        /// </summary>
+        public IDictionary<string, string> Preferences { get; set; }
+    }
+}
---
+++ simplic-preference/Simplic.Preference/Interface/IPreferenceService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Simplic.Preference
+{
+    /// <summary>
+    /// Preference service holds a list of key value pairs (Dictionary)
+    /// </summary>
+    public interface IPreferenceService
+    {
+        /// <summary>
+        /// Gets or sets the preferences
+        /// </summary>
+        IDictionary<string, string> Preferences { get; set; }
+    }
+}
