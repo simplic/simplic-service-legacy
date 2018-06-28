0a0dcef6aeeb2cdb4e5ca9b445d37b82a1fb417e
Yavuz Günay <yavuzgunay@gmail.com>
2018-06-28 15:41:28 +0200

    added login by api key

--- simplic-authentication/Simplic.Authentication.Service/Properties/AssemblyInfo.cs
+++ simplic-authentication/Simplic.Authentication.Service/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.2.18.626")]
-[assembly: AssemblyFileVersion("6.2.18.626")]
+[assembly: AssemblyVersion("6.2.118.628")]
+[assembly: AssemblyFileVersion("6.2.118.628")]
