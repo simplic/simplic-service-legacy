6c45ca67b0b80feebb09e237696169a40e2c6359
beggers <be@spiegelburg.net>
2018-07-19 14:00:04 +0200

    Fix bug where enumerator for stateful collection is wrong

--- simplic-corelib/Simplic.CoreLib/Properties/AssemblyInfo.cs
+++ simplic-corelib/Simplic.CoreLib/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.118.621")]
-[assembly: AssemblyFileVersion("6.1.118.621")]
+[assembly: AssemblyVersion("6.1.218.719")]
+[assembly: AssemblyFileVersion("6.1.218.719")]
