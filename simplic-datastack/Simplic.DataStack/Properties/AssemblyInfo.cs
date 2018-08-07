cb358f30d7e9484a742fc72babc6f8d1f85ae21e
beggers <be@spiegelburg.net>
2018-08-07 11:47:49 +0200

    Add stackname method

--- simplic-datastack/Simplic.DataStack/Properties/AssemblyInfo.cs
+++ simplic-datastack/Simplic.DataStack/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.1.118.807")]
-[assembly: AssemblyFileVersion("6.1.118.807")]
+[assembly: AssemblyVersion("6.1.218.807")]
+[assembly: AssemblyFileVersion("6.1.218.807")]
