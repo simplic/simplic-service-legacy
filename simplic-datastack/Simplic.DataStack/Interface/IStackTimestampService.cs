65b9b61155dadafe50de6e55203b59ab6d4fe849
beggers <be@spiegelburg.net>
2018-08-07 11:36:54 +0200

    Add teimstamp service

---
+++ simplic-datastack/Simplic.DataStack/Interface/IStackTimestampService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.DataStack
+{
+    /// <summary>
+    /// Stack timestamp service
+    /// </summary>
+    public interface IStackTimestampService
+    {
+        /// <summary>
+        /// Set instance data changed timestamp
+        /// </summary>
+        /// <param name="stackName">Stack name</param>
+        /// <param name="instanceDataId">Instance data id</param>
+        /// <returns>True if setting was successfull</returns>
+        bool SetTimestamp(string stackName, Guid instanceDataId);
+
+        /// <summary>
+        /// Set instance data changed timestamp
+        /// </summary>
+        /// <param name="stackId">Stack id</param>
+        /// <param name="instanceDataId">Instance data id</param>
+        /// <returns>True if setting was successfull</returns>
+        bool SetTimestamp(Guid stackId, Guid instanceDataId);
+
+        /// <summary>
+        /// Get all timestamp model
+        /// </summary>
+        /// <returns>Enumerable of timestamps</returns>
+        IEnumerable<InstanceDataTimestampModel> GetAll();
+
+        /// <summary>
+        /// Get timestamp by stack name
+        /// </summary>
+        /// <returns>Timestamp object or null</returns>
+        InstanceDataTimestampModel GetByStackName();
+
+        /// <summary>
+        /// Get timestamp by stack id
+        /// </summary>
+        /// <returns>Timestamp object or null</returns>
+        InstanceDataTimestampModel GetByStackId();
+    }
+}
---
+++ simplic-datastack/Simplic.DataStack/Model/InstanceDataTimeStamp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.DataStack
+{
+    /// <summary>
+    /// Instance data timestamp model
+    /// </summary>
+    public class InstanceDataTimestampModel
+    {
+        /// <summary>
+        /// Gets or sets the stack guid
+        /// </summary>
+        public Guid StackGuid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last changed instance data id
+        /// </summary>
+        public Guid LastInstanceDataGuid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the change datetime
+        /// </summary>
+        public DateTime ChangeDateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the change username
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the change user id
+        /// </summary>
+        public int UserId { get; set; }
+    }
+}
