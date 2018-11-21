21ef793171abd62e632909bd94ffcfd8420763d6
beggers <be@spiegelburg.net>
2018-11-21 23:00:05 +0100

    Add result object

--- simplic-viewer/Simplic.Viewer.Annotation/GenerateAnnotationResult.cs
+++ simplic-viewer/Simplic.Viewer.Annotation/GenerateAnnotationResult.cs
@@ -13,5 +13,23 @@
             get;
             set;
         }
+
+        /// <summary>
+        /// Gets or sets the error text
+        /// </summary>
+        public string ErrorText
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets whether generating was successfull
+        /// </summary>
+        public bool IsSuccessful
+        {
+            get;
+            set;
+        } = true;
     }
 }
