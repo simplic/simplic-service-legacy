a5bef600b426b3d5c5acf221499a3a784e44ace1
beggers <be@spiegelburg.net>
2018-11-21 22:50:43 +0100

    Annotation result

---
+++ simplic-viewer/Simplic.Viewer.Annotation/GenerateAnnotationResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Simplic.Viewer.Annotation
+{
+    /// <summary>
+    /// Generation result
+    /// </summary>
+    public class GenerateAnnotationResult
+    {
+        /// <summary>
+        /// Gets or sets the annotation text
+        /// </summary>
+        public string Text
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ simplic-viewer/Simplic.Viewer.Annotation/IGenerateAnnotationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Simplic.Viewer.Annotation
+{
+    /// <summary>
+    /// Interface for generating annotation that are basing on a given document
+    /// </summary>
+    public interface IGenerateAnnotationService
+    {
+        /// <summary>
+        /// Generate annotation result by using a blob guid
+        /// </summary>
+        /// <param name="blobId">Unique blob guid</param>
+        /// <returns>Annotation result</returns>
+        GenerateAnnotationResult Generate(Guid blobId);
+    }
+}
