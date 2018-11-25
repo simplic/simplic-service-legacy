1a644bc2b4f89b700bc75a225f9ef681eac799d4
beggers <be@spiegelburg.net>
2018-11-25 12:25:28 +0100

    Add events

---
+++ simplic-corelib/Simplic.CoreLib/Collections/Generic/StatefulCollection/ItemRemovedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Simplic.Collections.Generic
+{
+    public class ItemRemovedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets or sets the affected item
+        /// </summary>
+        public object Item
+        {
+            get;
+            set;
+        }
+    }
+}
