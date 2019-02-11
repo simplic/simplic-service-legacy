5a4299e8ae6d9be1ca397e869610971c38d46659
Lars Tepper <lars.tepper@simplic.biz>
2019-02-11 09:00:41 +0100

    ui

--- simplic-interval/Simplic.Interval.UI/Properties/AssemblyInfo.cs
+++ simplic-interval/Simplic.Interval.UI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.119.0123")]
-[assembly: AssemblyFileVersion("1.2.119.0123")]
+[assembly: AssemblyVersion("1.3.119.0211")]
+[assembly: AssemblyFileVersion("1.3.119.0211")]
