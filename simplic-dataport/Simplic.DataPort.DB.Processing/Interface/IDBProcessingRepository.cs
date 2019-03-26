3f11d3acfb093d08c025266f4fabb923fed00273
Yavuz Günay <yavuzgunay@gmail.com>
2019-03-26 15:50:58 +0100

    removed unnecessary service methods

--- simplic-dataport/Simplic.DataPort.DB.Processing/Interface/IDBProcessingRepository.cs
+++ simplic-dataport/Simplic.DataPort.DB.Processing/Interface/IDBProcessingRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 
 namespace Simplic.DataPort.DB.Processing
@@ -9,8 +10,10 @@ namespace Simplic.DataPort.DB.Processing
         bool ColumnExists(string tableName, string columnName, string connectionName = "default");
         void InsertOrUpdate(string transformerName, string tableName, DataRow row, string connectionName = "default");
         bool CreateTable(TableSchemaModel tableSchema, string connectionName = "default");
+
+        void LogTableError(TableSchemaModel tableSchema, Exception exception, string connectionName = "default");
+        void LogRowError(string tableName, string transformerName, DataRow row, Exception exception, string connectionName = "default");
         ErrorLogModel GetErrorLog(long id, string connectionName = "default");
-        IEnumerable<ErrorLogModel> GetAllErrorLog(string connectionName = "default");
-        bool Retry(ErrorLogModel errorLogModel, string connectionName = "default");
+        IEnumerable<ErrorLogModel> GetAllErrorLog(string connectionName = "default");        
     }
 }
--- simplic-dataport/Simplic.DataPort.DB.Processing/Interface/IDBProcessingService.cs
+++ simplic-dataport/Simplic.DataPort.DB.Processing/Interface/IDBProcessingService.cs
@@ -1,7 +1,32 @@
-﻿namespace Simplic.DataPort.DB.Processing
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Simplic.DataPort.DB.Processing
 {
-    public interface IDBProcessingService : IDBProcessingRepository
+    public interface IDBProcessingService
     {
-        
+        /// <summary>
+        /// Checks if table and columns exist, creates them if not and goes through the data and saves each data into the db. 
+        /// If any error is caused, will be logged to be retried later.
+        /// </summary>
+        /// <param name="transformerName">Transformer used to produce this db processing data</param>
+        /// <param name="data">Data to save</param>
+        /// <param name="connectionName">Connection Name</param>
+        void SaveData(string transformerName, FileTypeDBModel data, string connectionName = "default");
+
+        /// <summary>
+        /// Tries to save data again with the corrected sql or data. 
+        /// Updates the current error log if this one fails too. 
+        /// Deletes the error log entry if the corrected sql runs successfully.
+        /// </summary>
+        /// <param name="errorLogModel">Corrected data & sql</param>
+        /// <param name="connectionName">Connection Name</param>
+        void Retry(ErrorLogModel errorLogModel, string connectionName = "default");
+
+        void LogTableError(TableSchemaModel tableSchema, Exception exception, string connectionName = "default");
+        void LogRowError(string tableName, string transformerName, DataRow row, Exception exception, string connectionName = "default");
+        ErrorLogModel GetErrorLog(long id, string connectionName = "default");
+        IEnumerable<ErrorLogModel> GetAllErrorLog(string connectionName = "default");
     }
 }
