b3ff1ff87a1ba4622d6fa4cd35bad54deb6cff39
Yavuz Günay <yavuzgunay@gmail.com>
2019-03-26 15:50:37 +0100

    added exception

---
+++ simplic-dataport/Simplic.DataPort.DB.Processing/Exceptions/DBProcessingException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Simplic.DataPort.DB.Processing
+{
+    public class DBProcessingException : Exception
+    {
+        public DBProcessingException()
+        {
+        }
+
+        public DBProcessingException(string message) : base(message)
+        {
+        }
+
+        public DBProcessingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DBProcessingException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
