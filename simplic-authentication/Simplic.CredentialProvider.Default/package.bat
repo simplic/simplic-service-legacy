5a379bb7026a658ae044c37249cf9f94d91a492a
beggers <be@spiegelburg.net>
2020-03-22 11:00:54 +0100

    Add CI/CD for Auth

--- simplic-authentication/Simplic.CredentialProvider.Default/package.bat
+++
@@ -1,3 +0,0 @@
-del *.nupkg
-nuget pack Simplic.CredentialProvider.Default.csproj -properties Configuration=Debug
-nuget push *.nupkg -Source http://simplic.biz:10380/nuget
--- simplic-authorization/Simplic.Authorization/Properties/AssemblyInfo.cs
+++ simplic-authorization/Simplic.Authorization/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.2.218.824")]
-[assembly: AssemblyFileVersion("6.2.218.824")]
+[assembly: AssemblyVersion("6.3.20.322")]
+[assembly: AssemblyFileVersion("6.3.20.322")]
